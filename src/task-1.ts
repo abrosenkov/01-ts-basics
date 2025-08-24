feb7fc63802c27b486573670f9c00787baa608d6
abrosenkov <abrosenkov91@gmail.com>
2025-08-24 20:30:16 +0200

    add tasks

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,1 +1,7 @@
-console.log("Hello");
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
